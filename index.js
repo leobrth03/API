9cc209e7648d21970a830c046acb21ece1dec866
leobrth03 <leo.bertholet@ecole-isitech.fr>
2023-03-29 12:00:43 +0200

    ajouts API

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import mysql from 'mysql2';
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: '3306',
+    database: 'API',
+    password: 'notSecureChangeMe',
+
+}).once('connection', (stream) => {
+    console.log('Ah, we have our first user');
+});
+
 
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
-app.get ('/', function (httpRequest, httpResponse, next )
-{
-    //log de la requete entrante
-    console.log('objet request: ', httpRequest);
-    //envoie de la reponse http
-    httpResponse.render('pages/index')
+app.get('/', function (httpRequest, httpResponse, next) {
+    connection.query('select * from clients', (err, result, fields) => {
+        console.log(result)
+        //log de la requete entrante
+        console.log('objet request: ', httpRequest);
+        //envoie de la reponse http
+        httpResponse.render('pages/index')
+    })
+
 });
 
-app.get('/About_me', (req, res)=>{res.render('pages/About_me')});
-app.get('/Contact', (req, res)=>{res.render('pages/Contact')});
-app.get('/CV', (req, res)=>{res.render('pages/CV')});
-app.get('/index', (req, res)=>{res.render('pages/index')});
-app.get('/PPE', (req, res)=>{res.render('pages/PPE')});
-app.get('/Lettre_de_motivation', (req, res)=>{res.render('pages/Lettre_de_motivation')});
+app.get('/About_me', (req, res) => { res.render('pages/About_me') });
+app.get('/Contact', (req, res) => { res.render('pages/Contact') });
+app.get('/CV', (req, res) => { res.render('pages/CV') });
+app.get('/index', (req, res) => { res.render('pages/index') });
+app.get('/PPE', (req, res) => { res.render('pages/PPE') });
+app.get('/Lettre_de_motivation', (req, res) => { res.render('pages/Lettre_de_motivation') });
 app.listen('3003');
